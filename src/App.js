4fa9972466a8d597b3d229dd527a7fed513c13c2
ashu.9x@gmail.com <ashu.9x@gmail.com>
2020-04-17 19:31:21 +0530

    UI work

--- src/App.js
+++ src/App.js
@@ -1,77 +1,94 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-
-function Todo({index,todo,doAction}){
-  return(
-    <div className="todo" >
-      <input type="checkbox" checked={todo.isCompleted ? true : false }  onChange={e=>doAction(e.target.checked, index)} />
-      <div style={{textDecoration: todo.isCompleted ? 'line-through' : 'none',display:'inline' }}>{todo.text}</div>
-      
+function Todo({ index, todo, doAction }) {
+  return (
+    <div className="course-feature animate-in" id="">
+      <p>
+        <input
+          type="checkbox"
+          checked={todo.isCompleted ? true : false}
+          onChange={e => doAction("checked", e.target.checked, index)}
+        />
+        <span
+          style={{
+            textDecoration: todo.isCompleted ? "line-through" : "none",
+            display: "inline"
+          }}
+        >
+          {todo.text}
+        </span>
+        <button onClick={e => doAction("delete", false, index)}>Delete</button>
+      </p>
     </div>
-  )
-
+  );
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
 
-function TodoForm({addTodo}){
-  const [value,setValue] = useState('');
-
-  const handleSubmit = e =>{
+  const handleSubmit = e => {
     e.preventDefault();
-     if(!value)
-     return;
-     addTodo(value);
-     setValue('');
-  }
-  return(
-
-    
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+  return (
     <form onSubmit={handleSubmit}>
-      <div className="form-group" >
-      <input type="text" className="form-control" id="todo"  value={value} onChange={e=>setValue(e.target.value)}/>
-    </div>
+      <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          id="todo"
+          value={value}
+          placeholder="Add task here ..."
+          onChange={e => setValue(e.target.value)}
+        />
+      </div>
     </form>
-  )
- 
+  );
 }
 
 function App() {
+  const preList = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+  const [todos, setTodos] = useState(preList);
 
-    const preList = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []
-    const[todos,setTodos]  = useState(preList);
+  useEffect(() => {
+    console.log("added......");
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    useEffect(()=>{
-      console.log('added......');
-      localStorage.setItem('todos',JSON.stringify(todos) );
-    },[todos]);
-
-
-    const addTodo = text =>{
-      const  newTodos = [...todos, {text:text, isCompleted:false}];
-      console.log(newTodos);
-      setTodos(newTodos);
+  const addTodo = text => {
+    const newTodos = [...todos, { text: text, isCompleted: false }];
+    console.log(newTodos);
+    setTodos(newTodos);
+  };
+  const handleAction = (action, status, index) => {
+    const newTodos = [...todos];
+    if (action == "checked") {
+      newTodos[index].isCompleted = status;
     }
-    const handleAction = (value,index) => {
-      const  newTodos = [...todos];
-      newTodos[index].isCompleted = value
-      setTodos(newTodos);
+    if (action == "delete") {
+      newTodos.splice(index, 1);
     }
+    setTodos(newTodos);
+  };
 
-  return(
+  return (
     <div className="app">
-      <h1>Welcome to To Do List</h1>
-      <div className='todo-list'>
-        {
-          todos.map((todo,index) => (
-            <Todo key={index} index={index} todo={todo} doAction={handleAction}/>
-        ))
+      <div className="title animate-in">
+        <h1>To do list ...</h1>
+      </div>
 
-        }
+      <div className="todo-list" style={{ padding: "20px" }}>
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} doAction={handleAction} />
+        ))}
         <TodoForm addTodo={addTodo} />
-      </div> 
-
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
